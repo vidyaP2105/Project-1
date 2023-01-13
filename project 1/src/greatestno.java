0ad0cc7d0d93dde50016ae063d1dd0e3d9c249d6
Admin <Admin@DESKTOP-NRGEUNT>
2023-01-13 22:27:53 +0530

    userstory project1

---
+++ project 1/src/greatestno.java
@@ -0,0 +1,19 @@
+
+public class greatestno {
+	public void main( String[]args) {
+		  
+        int a = 10;
+        int b =20;
+
+if(a>b)
+{
+	 System.out.println("a is graeter");
+}
+
+else
+{
+	 System.out.println("b is greater");
+}
+}
+
+}
